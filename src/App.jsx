4b31e32e87ec4d88809a5300bfd5ac3eb4470222
Rishabh Jain <Rishabh2824@email.com>
2025-06-27 01:52:51 -0500

    Added About, Home UI pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,30 @@
-import { useState } from "react";
+import {useState} from "react";
 import "./App.css";
 import AIHStudio from "./components/home/AIHStudio";
 import AIHAgents from "./components/home/AIHAgents";
 import SuccessStory from "./components/home/SuccessStory";
 import Blogs from "./components/home/Blogs";
+import {APIProvider} from "@vis.gl/react-google-maps";
+
 // import Home from "./pages/Home";
 
 function App() {
-  const [count, setCount] = useState(0);
+    const [count, setCount] = useState(0);
 
-  return (
-    <>
-      <SuccessStory />
-      <AIHAgents />
-      <AIHStudio />
-      <Blogs />
-      {/* <Home /> */}
-    </>
-  );
+    return (
+        <>
+            <SuccessStory/>
+            <AIHAgents/>
+            <AIHStudio/>
+            <Blogs/>
+            {/* <Home /> */}
+
+            {/* TODO: Refactor the API key - Google maps support for last home page */}
+            {/*<APIProvider apiKey={AIzaSyAWpGF4YuL4EUS37-tUy0xOeldVIKkVnVY}>*/}
+            {/*<HomeLast />*/}
+            {/*</APIProvider>*/}
+        </>
+    );
 }
 
 export default App;
---
+++ src/pages/HomeLast.jsx
@@ -0,0 +1,99 @@
+import React from 'react';
+import '../css/HomeLast.css';
+import {GiSpinningTop} from "react-icons/gi";
+import ailogo from "../assets/ailogo.png"
+import {Map, Marker} from "@vis.gl/react-google-maps";
+import {FaLinkedin} from "react-icons/fa6";
+
+const HomeLast = () => {
+    const center = { lat: 19.1139043, lng: 72.9181878 };
+
+
+    return (<footer className="footer">
+        <div className="footer-top">
+            <div className="email-container">
+                <span className="email-placeholder">Enter your Email ID</span>
+                <button className="submit-button">Submit</button>
+            </div>
+
+            <h2 className="footer-title">Build your own private GenAI Now!</h2>
+        </div>
+
+        <div className="breadcrumb-section">
+            <GiSpinningTop className="breadcrumb-icon"/>
+            <span className="breadcrumb-text">> AI Horizons > Home</span>
+        </div>
+
+        <div className="quick-links-title">
+            Quick Links
+        </div>
+
+        <div className="links-grid">
+            <div className="links-column">
+                <h3 className="column-title">Company</h3>
+                <ul className="links-list">
+                    <li><a href="">About us</a></li>
+                    <li><a href="">Partner</a></li>
+                    <li><a href="">Strategic Investor</a></li>
+                    <li><a href="">Leadership</a></li>
+                </ul>
+            </div>
+            <div className="links-column">
+                <h3 className="column-title">Resources</h3>
+                <ul className="links-list">
+                    <li><a href="">Blogs</a></li>
+                    <li><a href="">Case Studies</a></li>
+                    <li><a href="">Comparisons</a></li>
+                    <li><a href="">Industries</a></li>
+                    <li><a href="">User Cases</a></li>
+                    <li><a href="">Newsrooms</a></li>
+                </ul>
+            </div>
+            <div className="links-column">
+                <h3 className="column-title">Platform</h3>
+                <ul className="links-list">
+                    <li><a href="">Career</a></li>
+                    <li><a href="">Solutions</a></li>
+                    <li><a href="">Enterprises</a></li>
+                </ul>
+            </div>
+        </div>
+
+
+        <div className="logo-container">
+            <img src={ailogo} alt="AI Logo" className="ai-logo"/>
+        </div>
+
+        <div
+            className="map-container"
+            onClick={() => window.open('https://www.google.com/maps/place/KAILASH+BUSINESS+PARK,+Park+Site+Rd,+HMPL+Surya+Nagar,+Vikhroli+West,+Mumbai,+Maharashtra+400076/@19.1139446,72.9154082,17z/data=!3m1!4b1!4m6!3m5!1s0x3be7c7ea348f1cd3:0x9f0d02f9fba7f991!8m2!3d19.1139043!4d72.9181878!16s%2Fg%2F11bw3fnc5q?entry=ttu&g_ep=EgoyMDI1MDYyMi4wIKXMDSoASAFQAw%3D%3D', +'_blank')}
+        >
+            <Map
+                defaultZoom={13}
+                defaultCenter={center}
+                gestureHandling="greedy"
+                disableDefaultUI
+            >
+                <Marker position={center}/>
+            </Map>
+        </div>
+
+        <div className="contact-section">
+            <h3 className="contact-title">Contact</h3>
+            <p className="contact-address">
+                1306-07, C wing, Kailas Business Park, HMPL<br/>
+                Surya Nagar, Vikhroli West, Mumbai,<br/>
+                Maharashtra 400079
+            </p>
+            <FaLinkedin className="linkedin-icon"/>
+        </div>
+
+        <div className="footer-bottom-text">
+            <a href="/privacy" className="footer-link">Privacy Policy</a> |
+            <a href="/terms" className="footer-link"> Terms and Conditions</a> |
+            India
+        </div>
+    </footer>);
+};
+
+export default HomeLast;
