095c0c5a35a0f7ff2a0f46e0568952d28dbdd848
Ranjan Chauhan <chauhanranjan8935@gmail.com>
2025-06-27 17:34:15 +0530

    itegrating all the pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,34 @@
-import {useState} from "react";
+import { useState } from "react";
 import "./App.css";
 import AIHStudio from "./components/home/AIHStudio";
 import AIHAgents from "./components/home/AIHAgents";
 import SuccessStory from "./components/home/SuccessStory";
 import Blogs from "./components/home/Blogs";
-import {APIProvider} from "@vis.gl/react-google-maps";
+import { APIProvider } from "@vis.gl/react-google-maps";
+import HomePage from "./components/home/HomePage";
+import HomeLast from "./components/home/HomeLast";
 
 // import Home from "./pages/Home";
 
 function App() {
-    const [count, setCount] = useState(0);
+  const [count, setCount] = useState(0);
 
-    return (
-        <>
-            <SuccessStory/>
-            <AIHAgents/>
-            <AIHStudio/>
-            <Blogs/>
-            {/* <Home /> */}
+  return (
+    <>
+      <HomePage />
+      <SuccessStory />
+      <AIHAgents />
+      <AIHStudio />
+      <Blogs />
+      {/* <HomeLast /> */}
+      {/* <Home /> */}
 
-            {/* TODO: Refactor the API key - Google maps support for last home page */}
-            {/*<APIProvider apiKey={AIzaSyAWpGF4YuL4EUS37-tUy0xOeldVIKkVnVY}>*/}
-            {/*<HomeLast />*/}
-            {/*</APIProvider>*/}
-        </>
-    );
+      {/* TODO: Refactor the API key - Google maps support for last home page */}
+      {/*<APIProvider apiKey={AIzaSyAWpGF4YuL4EUS37-tUy0xOeldVIKkVnVY}>*/}
+      {/*<HomeLast />*/}
+      {/*</APIProvider>*/}
+    </>
+  );
 }
 
 export default App;
---
+++ src/pages/About.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import About1 from "../components/about/About1";
+import About2 from "../components/about/About2";
+
+const About = () => {
+  return (
+    <>
+      <About1 />
+      <About2 />
+    </>
+  );
+};
+
+export default About;
